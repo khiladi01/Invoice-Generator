bca5e231001023ba7fee6df342c6961d0e8e2d7e
raushan778 <allmusiccenter404@gmail.com>
2025-04-25 01:10:03 +0530

    updated

--- invoice-generator/src/App.jsx
+++ invoice-generator/src/App.jsx
@@ -1,24 +1,25 @@
-import { useState } from 'react'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import SenderForm from './components/SenderForm';
+import ClientForm from './components/ClientForm';
+import bgImage from './assets/bgimg.jpg';
 
 function App() {
 
-  const [InvoiceData , setInvoiceData] = useState({
-    sender : {name: "" , address : "" , phone : "" , email : "" , website : ""},
-    client: {name : "" , address : "" , phone : "" , email : "" , website : ""},
-    items : [],
-    discount : 0,
-    notes : ""
-  });
 
   return (
-    <>
-      <div className='bg-gray-500 h-screen w-full grid place-content-center'>
-        <SenderForm 
-          sender={InvoiceData.sender}
-          setInvoiceData={setInvoiceData}
-        />
-      </div>
+    <>  
+    {/* We use backticks to declare bgImage varaible*/}
+
+        <div className='h-screen w-screen bg-black flex justify-center items-center bg-cover bg-center' style={{backgroundImage : `url(${bgImage})` }} > 
+          
+          <div className='h-[500px] w-[600px] flex justify-center items-center border-0 border-cyan-700 bg-transparent rounded-lg shadow-md shadow-cyan-800'>
+           <Routes>
+              <Route path='/' element={<SenderForm />} />
+              <Route path='/client' element={<ClientForm />} />
+           </Routes>
+           </div>
+           </div>
     </>
   )
 }
---
+++ invoice-generator/src/components/ClientForm.jsx
@@ -0,0 +1,80 @@
+import { useState } from 'react'
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+function ClientForm({
+    name, setName, address, setAddress, email, setEmail, phone, setphone, website, setWebsite
+}) {
+
+    const [error , setError] = useState("") 
+    
+    const handleerror = (e) => {
+        e.preventDefault();
+
+        if (name  === ""){
+            setError("Name Is Required")
+            return;
+        }else{
+            console.log("Sender Data :" , {name,address,email,phone,website});
+        }
+        
+    }
+
+    return (
+        <>
+        <div className="flex justify-center items-center min-h-screen px-4">
+            <div className="w-full max-w-sm">
+                <header className='h-[30px] w-full flex justify-center items-center bg-transparent text-cyan-50 font-bold text-lg p-5 underline-offset-8 underline decoration-2 decoration-cyan-600'>
+                    <p>Client Form</p>
+                </header>
+                <form onSubmit={handleerror}>
+                    <input
+                        type="text"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder="Sender Name"
+                        className='h-[40px] w-full border-1 border-cyan-600 p-4 rounded-lg m-1 text-slate-800 font-medium outline-0' />
+                    <input
+                        type="address"
+                        value={address}
+                        onChange={(e) => setAddress(e.target.value)}
+                        placeholder="Sender Address"
+                        className='h-[40px] w-full border-1 border-cyan-600 p-4 rounded-lg m-1 text-slate-800 font-medium outline-0' />
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Sender Email"
+                        className='h-[40px] w-full border-1 border-cyan-600 p-4 rounded-lg m-1 text-slate-800 font-medium outline-0' />
+                    <input
+                        type="text"
+                        value={phone}
+                        onChange={(e) => setphone(e.target.value)}
+                        placeholder="Sender Phone"
+                        className='h-[40px] w-full border-1 border-cyan-600 p-4 rounded-lg m-1 text-slate-800 font-medium outline-0' />
+                    <input
+                        type="text"
+                        value={website}
+                        onChange={(e) => setWebsite(e.target.value)}
+                        placeholder="Put Your website URL (Optional)"
+                        className='h-[40px] w-full border-1 border-cyan-600 p-4 rounded-lg m-1 text-slate-800 font-medium outline-0 italic' />
+                    <input
+                        type="submit"
+                        value="Submit"
+                        className='h-[60px] w-full border-1 border-cyan-600 p-4 rounded-lg flex justify-center items-center m-1 hover:bg-cyan-100 hover:text-black font-medium cursor-pointer outline-0' />
+
+                    {error && <p className='text-red-500 h-[30px] text-left p-1'> {error} </p>}
+
+                    <Link
+                        to="/"
+                        className="h-[30px] w-[148px] block text-left text-slate-800 p-1.5 font-medium outline-0 rounded-lg m-1  hover:text-cyan-600 cursor-pointer "
+                    >
+                        Go to Sender Page
+                    </Link>
+                </form>
+            </div>
+        </div>
+        </>
+    );
+}
+export default ClientForm;
